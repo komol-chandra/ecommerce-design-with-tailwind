9a0809d087f0cbd774761e45b3a7cb645b764b0d
komol-chandra <kiri2kaofficial@gmail.com>
2021-10-17 14:39:30 +0600

    font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: "'Poppins', sans-serif",
+				roboto: "'Roboto', sans-serif"
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
